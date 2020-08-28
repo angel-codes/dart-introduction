9c54d90aba61b501107e3ff88e34994be3ee12e9
Angel Martinez <65056245+angel-codes@users.noreply.github.com>
2020-08-27 20:48:30 -0500

    Data types - Numbers and Strings

---
+++ data_types-numbers_and_strings.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Numbers
+  int employees = 10;
+  double salary = 132.43;
+  
+  print('$employees with Salary of: $salary per Day');
+  
+  // Strings
+  String name = 'Angel';
+  print(name[0]); // first letter of the string
+  print(name[name.length - 1]); // last letter
+}
